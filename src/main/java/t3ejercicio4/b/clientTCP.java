e4a0badb3763952f9eedbf35065d8818877c6082
guicamher <guillecampher15@gmail.com>
2026-01-12 16:33:46 +0100

    a

---
+++ src/main/java/t3ejercicio4/b/clientTCP.java
@@ -0,0 +1,32 @@
+package t3ejercicio4.b;
+
+import java.io.*;
+import java.net.Socket;
+
+public class clientTCP {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 12345);
+
+            try {
+                InputStream inputStream =
+                        socket.getInputStream();
+                BufferedReader in = new BufferedReader(new
+                        InputStreamReader(inputStream));
+                String respuesta = in.readLine();
+                System.out.println("Mensaje del cliente: " +
+                        respuesta);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter out = new PrintWriter(outputStream,
+                    true);
+            out.println("Hola, servidor!");
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
